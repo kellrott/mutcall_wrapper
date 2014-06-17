8e4224691cd9c4d633ad5d26bc658c5d6bc94faa
Kyle Ellrott <kellrott@gmail.com>
2014-06-17 12:20:06 -0700

    Upgrading mutect version

--- tools/mutect_install.sh
+++ tools/mutect_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 
-wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/muTect-1.1.4-bin.zip 
+wget https://github.com/broadinstitute/mutect/releases/download/1.1.5/muTect-1.1.5-bin.zip
 wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/b37_cosmic_v54_120711.vcf
 wget http://www.broadinstitute.org/cancer/cga/sites/default/files/data/tools/mutect/dbsnp_132_b37.leftAligned.vcf.gz
-unzip muTect-1.1.4-bin.zip 
+unzip muTect-1.1.5-bin.zip 
